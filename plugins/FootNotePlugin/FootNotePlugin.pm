6308d7540ed221688fd8212ec4011210683ab94e
ian <ian>
2007-04-29 23:41:55 +0000

    Bump version.

--- plugins/FootNotePlugin/FootNotePlugin.pm
+++ plugins/FootNotePlugin/FootNotePlugin.pm
@@ -25,7 +25,7 @@ use vars qw(
         $debug @notes $header $footer $maintopic
     );
 
-$VERSION = '1.1';
+$VERSION = '2.001';
 $pluginName = 'FootNotePlugin';  # Name of this Plugin
 
 # =========================
